3a52e002a1383598ad1b95abe77f7fc7c87a9f38
Didier Defrance <didier@d-defrance.fr>
2023-06-12 15:52:08 +0200

    aa

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		System.out.println("projetJavaEnEquipe");
-		System.out.println("avec branches main et dev");
+		System.out.println("avec branches main et dev **");
 	}
 
 }
