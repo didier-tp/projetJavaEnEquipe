f7db736655d05bb860869f84d5345f456601b181
Didier Defrance <didier@d-defrance.fr>
2023-06-12 15:04:49 +0200

    initial

---
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -0,0 +1,10 @@
+package com.m2i.tp;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		System.out.println("projetJavaEnEquipe");
+		System.out.println("avec branches main et dev");
+	}
+
+}
